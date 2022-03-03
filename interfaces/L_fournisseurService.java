a44058313c052fbe6063b8df0bec715ece3f8cb9
abirm74 <abir.moula@esprit.tn>
2022-03-03 10:48:58 +0100

    1st push module fournisseur

---
+++ interfaces/L_fournisseurService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+
+import entities.fournisseur;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author abyrm
+ */
+public interface L_fournisseurService <T> {
+           void ajouterFournisseur (T entity) throws SQLException ; 
+    void modifierFournisseur(int id_fournisseur , T t) throws SQLException ; 
+     void supprimerFournisseur (int id_fournisseur) throws SQLException;
+        List<T> afficherFournisseur();
+        
+}
