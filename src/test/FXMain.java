d9cf0cf65d42e6fb984ced27c93a91e6aaa359e3
Jallouz-Nesrine <81320836+Jallouz-Nesrine@users.noreply.github.com>
2022-03-11 10:38:53 +0100

    Module_Facture_Version_Finale

--- src/test/FXMain.java
+++ src/test/FXMain.java
@@ -27,7 +27,7 @@ public class FXMain extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("../gui/ListeFacture.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("../gui/CmdFournisseur.fxml"));
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
